944be71dda96a5b842a848595f2222af71644cef
riacd <1927208797@qq.com>
2023-05-21 00:38:30 +0800

    update

--- pages/2_🕸_服务器.py
+++ pages/2_🕸_服务器.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from PIL import Image
+
 
 st.write("# 模糊提取器")
 server_id_list = ['1', '2', '3', '4']
@@ -6,7 +8,7 @@ server_id_list = ['1', '2', '3', '4']
 st.sidebar.write('配置')
 server_id = st.sidebar.selectbox('服务器ID', server_id_list)
 
-protocol_tab, data_tab, message_tab, feature_extraction_tab = st.tabs(["协议运行", "数据库", "消息记录", "特征提取"])
+protocol_tab, data_tab, message_tab, feature_extraction_tab, fuzzy_extraction_tab= st.tabs(["协议运行", "数据库", "消息记录", "特征提取", "射频指纹提取"])
 
 with protocol_tab:
     "服务器端只会被动运行协议，接收到用户终端发送请求后进行处理及消息返回"
@@ -21,6 +23,11 @@ with message_tab:
 with feature_extraction_tab:
     '这部分显示放深度学习部分相关数据'
 
+with fuzzy_extraction_tab:
+    st.write("# 混淆矩阵图如下")
+
+    image = Image.open('conf_mat.png')
+    st.image(image, caption='Confusion Matrix', use_column_width=True)
 # # Or even better, call Streamlit functions inside a "with" block:
 # with right_column:
 #     chosen = st.radio(
